6058ce959b8843267284f1da5da8eeb25964d7cd
itsSubroto <subratasantra9638@gmail.com>
2024-02-17 16:12:30 +0530

    background img added through js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const yesBtn = document.querySelector("#yesBtn");
 const noBtn = document.querySelector("#noBtn");
+const bg = document.querySelector(".container");
 
 yesBtn.addEventListener("click", () => {
   alert("Thank you!!! I will def be your valentine");
+//   bg.style.background = 'url("couple.jpg")';
+  bg.style.background = 'url("couple.jpg")';
 });
 
 noBtn.addEventListener("mouseover", () => {
-  let top = Math.floor(Math.random() *90);
-    let left = Math.floor(Math.random() * 90);
-    
-    console.log(top);
-    console.log(left);
+  let top = Math.floor(Math.random() * 90);
+  let left = Math.floor(Math.random() * 90);
 
-    noBtn.style.position = 'absolute'
-    noBtn.style.top=`${top}%`
-    noBtn.style.left=`${left}%`
+  console.log(top);
+  console.log(left);
 
+  noBtn.style.position = "absolute";
+  noBtn.style.top = `${top}%`;
+  noBtn.style.left = `${left}%`;
 });
